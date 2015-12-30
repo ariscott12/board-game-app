358186522d811c78381d07722645dc3eacd38090
arsscot12 <ariscot12@gmail.com>
2015-12-31 04:26:57 +0700

    created actions for calling methods in store

---
+++ src/actions.jsx
@@ -0,0 +1,7 @@
+var Reflux = require('reflux');
+
+module.exports = Reflux.createActions([
+	// single action getTopics, runs getTopics in any store that has getTopics method
+	'getHotGames',
+	'getGameDetails'
+]);
