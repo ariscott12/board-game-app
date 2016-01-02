9e551437e8317e75e65bb14836a9b14836456a07
arsscot12 <ariscot12@gmail.com>
2016-01-02 13:56:28 +0700

    added methods to store to fetch game data and search through existing game array and return specific object, also converted to es6

--- src/stores/game-detail-store.jsx
+++ src/stores/game-detail-store.jsx
@@ -1,21 +1,43 @@
-var Api = require('../utils/api');
-var Reflux = require('reflux');
-var Actions = require('../actions');
+"use strict";
+
+const Api = require('../utils/api');
+const Reflux = require('reflux');
+const Actions = require('../actions');
+const _ = require('underscore');
 
 module.exports = Reflux.createStore({
 	// listen to any available actions provided by actions.jsx, if action matches method call it
 	listenables: [Actions],
-	getGameDetails: function(gameIds) {
-		// fires ajax request using api module (api.jsx)
+	gameIds: [],
+	games: [],
+	getGameDetails(gameIds) {
+		let difference = _.difference(gameIds,this.gameIds);
 
+		if(difference.length > 0) {
+			this.gameIds = this.gameIds.concat(difference,this.gameIds);
+			this.fetchGameData(difference);
+		}  else {
+			this.triggerChange();	
+		}
+	},
+	fetchGameData(gameIds) {
+		// fires ajax request using api module (api.jsx)
 		return Api.get('thing?id=' + gameIds + '&stats=1')
 			.then(function(json) {
-      			this.games = json.items.item
-				this.triggerChange();	 
-    		}.bind(this));
-		
+  				this.games = this.games.concat(json.items.item);
+				this.triggerChange();	
+		}.bind(this));
 	},
-	triggerChange: function() {
+	findGame(id) {
+		for (var i = 0; i < this.games.length; i++) {
+			if(this.games[i].$.id === id) {
+				return [this.games[i]];
+			} else {
+				this.getGameDetails(id);
+			}
+		}
+	},
+	triggerChange() {
 		// fire trigger event
 		this.trigger('change', this.games);
 	}
